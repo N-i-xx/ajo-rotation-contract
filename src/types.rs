1192ceb1320507bd1a6c5277f03845f9dad49326
N-i-xx <nixchimaobi@gmail.com>
2026-05-19 01:44:54 +0000

    feat(ajo): introduce contract error enums providing context failures

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,4 @@
 pub struct Circle { pub amount: i128, pub round: u32, pub total_slots: u32 }
 // Storage mapping definitions
 // Refactoring state key layouts
+pub enum Error { CycleFull = 1, EarlyDrawAttempt = 2 }
